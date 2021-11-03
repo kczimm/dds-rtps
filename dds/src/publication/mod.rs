122bbe74b35235321e2757d8c4eceeb8a172f451
Kevin C. Zimmerman <kevinczimmerman@gmail.com>
2021-11-03 17:43:36 -0500

    start Publisher

--- dds/src/publication/mod.rs
+++ dds/src/publication/mod.rs
@@ -2,5 +2,22 @@
 //! as the PublisherListener and DataWriterListener interfaces, and more
 //! generally, all that is needed on the publication side.
 
-#[derive(Debug, PartialEq, PartialOrd, Hash, Eq, Ord)]
+use crate::{qos::QosPolicy, topic::Topic};
+
+#[derive(Debug)]
 pub struct Publisher;
+
+impl Publisher {
+    pub fn new() -> Self {
+        todo!()
+    }
+
+    pub fn create_datawriter<D>(&mut self, _topic: &Topic, _qos: &QosPolicy) -> DataWriter<D> {
+        todo!()
+    }
+}
+
+#[derive(Debug)]
+pub struct DataWriter<Data> {
+    _phantom: std::marker::PhantomData<Data>,
+}
