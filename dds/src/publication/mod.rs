6c41c90ce2a1553a9d0e2a5c392c30d6650df192
Kevin C. Zimmerman <kevinczimmerman@gmail.com>
2021-11-03 19:19:49 -0500

    stub out Publisher impl

--- dds/src/publication/mod.rs
+++ dds/src/publication/mod.rs
@@ -15,6 +15,54 @@ impl Publisher {
     pub fn create_datawriter<D>(&mut self, _topic: &Topic, _qos: &QosPolicy) -> DataWriter<D> {
         todo!()
     }
+
+    pub fn delete_datawriter(&mut self) {
+        todo!()
+    }
+
+    pub fn lookup_datawriter(&self, _topic_name: &str) {
+        todo!()
+    }
+
+    pub fn suspend_publications(&mut self) {
+        todo!()
+    }
+
+    pub fn resume_publications(&mut self) {
+        todo!()
+    }
+
+    pub fn begin_coherent_changes(&mut self) {
+        todo!()
+    }
+
+    pub fn end_coherent_changes(&mut self) {
+        todo!()
+    }
+
+    pub fn wait_for_acknowledgements(&mut self, _max_wait: std::time::Duration) {
+        todo!()
+    }
+
+    pub fn get_participant(&self) {
+        todo!()
+    }
+
+    pub fn delete_contained_entities(&mut self) {
+        todo!()
+    }
+
+    pub fn set_default_datawriter_qos(&mut self, _qos_list: &Vec<QosPolicy>) {
+        todo!()
+    }
+
+    pub fn get_default_datawriter_qos(&self) -> &Vec<QosPolicy> {
+        todo!()
+    }
+
+    pub fn copy_from_topic_qos(&self) {
+        todo!()
+    }
 }
 
 #[derive(Debug)]
