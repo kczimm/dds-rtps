ea8b0d3de87717f90d6d64f61d4b8928551ff481
Kevin C. Zimmerman <kevinczimmerman@gmail.com>
2021-11-04 11:36:23 -0500

    redesign QoS

--- dds/src/qos/mod.rs
+++ dds/src/qos/mod.rs
@@ -3,83 +3,147 @@
 
 use std::cmp::Ordering;
 
-pub enum QosPolicy {
-    UserData {
-        value: Vec<u8>,
-    },
-    TopicData {
-        value: Vec<u8>,
-    },
-    GroupData {
-        datavalue: Vec<u8>,
-    },
-    Durability {
-        kind: DurabilityKind,
-    },
-    DurabilityService {
-        service_cleanup_delay: Duration,
-        history_kind: HistoryKind,
-        history_depth: i32,
-        max_samples: i32,
-        max_instances: i32,
-        max_samples_per_instance: i32,
-    },
-    Presentation {
-        access_scope: PresentationAccessScopeKind,
-        coherent_access: bool,
-        ordered_access: bool,
-    },
-    Deadline {
-        period: Duration,
-    },
-    LatencyBudget {
-        period: Duration,
-    },
-    Ownership {
-        kind: OwnershipKind,
-    },
-    Liveliness {
-        lease_duration: Duration,
-        kind: LivelinessKind,
-    },
-    TimeBasedFilter {
-        minimum_separation: Duration,
-    },
-    Partition {
-        name: String,
-    },
-    Reliability {
-        kind: ReliabilityKind,
-        max_blocking_time: Duration,
-    },
-    TransportPriority {
-        value: i32,
-    },
-    Lifespan {
-        duration: Duration,
-    },
-    DestinationOrder {
-        kind: DestinationOrderKind,
-    },
-    History {
-        kind: HistoryKind,
-        depth: i32,
-    },
-    ResourceLimits {
-        max_samples: i32,
-        max_instances: i32,
-        max_samples_per_instance: i32,
-    },
-    EntityFactory {
-        autoenable_created_entities: bool,
-    },
-    WriterDataLifecycle {
-        autodispose_unregistered_instances: bool,
-    },
-    ReaderDataLifecycle {
-        autopurge_nowriter_samples_delay: Duration,
-        autopurge_disposed_samples_delay: Duration,
-    },
+#[derive(Debug, PartialEq, PartialOrd, Hash, Eq, Ord)]
+pub struct QosPolicy {
+    user_data: Option<UserData>,
+    topic_data: Option<TopicData>,
+    group_data: Option<GroupData>,
+    durability: Option<Durability>,
+    durability_service: Option<DurabilityService>,
+    presentation: Option<Presentation>,
+    deadline: Option<Deadline>,
+    latency_budget: Option<LatencyBudget>,
+    ownership: Option<Ownership>,
+    liveliness: Option<Liveliness>,
+    time_based_filter: Option<TimeBasedFilter>,
+    partition: Option<Partition>,
+    reliability: Option<Reliability>,
+    transport_priority: Option<TransportPriority>,
+    lifespan: Option<Lifespan>,
+    destination_order: Option<DestinationOrder>,
+    history: Option<History>,
+    resource_limits: Option<ResourceLimits>,
+    entity_factory: Option<EntityFactory>,
+    writer_data_lifecycle: Option<WriterDataLifecycle>,
+    reader_data_lifecycle: Option<ReaderDataLifecycle>,
+}
+
+#[derive(Debug, PartialEq, PartialOrd, Hash, Eq, Ord)]
+pub struct UserData {
+    value: Vec<u8>,
+}
+
+#[derive(Debug, PartialEq, PartialOrd, Hash, Eq, Ord)]
+pub struct TopicData {
+    value: Vec<u8>,
+}
+
+#[derive(Debug, PartialEq, PartialOrd, Hash, Eq, Ord)]
+pub struct GroupData {
+    datavalue: Vec<u8>,
+}
+
+#[derive(Debug, PartialEq, PartialOrd, Hash, Eq, Ord)]
+pub struct Durability {
+    kind: DurabilityKind,
+}
+
+#[derive(Debug, PartialEq, PartialOrd, Hash, Eq, Ord)]
+pub struct DurabilityService {
+    service_cleanup_delay: Duration,
+    history_kind: HistoryKind,
+    history_depth: i32,
+    max_samples: i32,
+    max_instances: i32,
+    max_samples_per_instance: i32,
+}
+
+#[derive(Debug, PartialEq, PartialOrd, Hash, Eq, Ord)]
+pub struct Presentation {
+    access_scope: PresentationAccessScopeKind,
+    coherent_access: bool,
+    ordered_access: bool,
+}
+
+#[derive(Debug, PartialEq, PartialOrd, Hash, Eq, Ord)]
+pub struct Deadline {
+    period: Duration,
+}
+
+#[derive(Debug, PartialEq, PartialOrd, Hash, Eq, Ord)]
+pub struct LatencyBudget {
+    period: Duration,
+}
+
+#[derive(Debug, PartialEq, PartialOrd, Hash, Eq, Ord)]
+pub struct Ownership {
+    kind: OwnershipKind,
+}
+
+#[derive(Debug, PartialEq, PartialOrd, Hash, Eq, Ord)]
+pub struct Liveliness {
+    lease_duration: Duration,
+    kind: LivelinessKind,
+}
+
+#[derive(Debug, PartialEq, PartialOrd, Hash, Eq, Ord)]
+pub struct TimeBasedFilter {
+    minimum_separation: Duration,
+}
+
+#[derive(Debug, PartialEq, PartialOrd, Hash, Eq, Ord)]
+pub struct Partition {
+    name: String,
+}
+
+#[derive(Debug, PartialEq, PartialOrd, Hash, Eq, Ord)]
+pub struct Reliability {
+    kind: ReliabilityKind,
+    max_blocking_time: Duration,
+}
+
+#[derive(Debug, PartialEq, PartialOrd, Hash, Eq, Ord)]
+pub struct TransportPriority {
+    value: i32,
+}
+
+#[derive(Debug, PartialEq, PartialOrd, Hash, Eq, Ord)]
+pub struct Lifespan {
+    duration: Duration,
+}
+
+#[derive(Debug, PartialEq, PartialOrd, Hash, Eq, Ord)]
+pub struct DestinationOrder {
+    kind: DestinationOrderKind,
+}
+
+#[derive(Debug, PartialEq, PartialOrd, Hash, Eq, Ord)]
+pub struct History {
+    kind: HistoryKind,
+    depth: i32,
+}
+
+#[derive(Debug, PartialEq, PartialOrd, Hash, Eq, Ord)]
+pub struct ResourceLimits {
+    max_samples: i32,
+    max_instances: i32,
+    max_samples_per_instance: i32,
+}
+
+#[derive(Debug, PartialEq, PartialOrd, Hash, Eq, Ord)]
+pub struct EntityFactory {
+    autoenable_created_entities: bool,
+}
+
+#[derive(Debug, PartialEq, PartialOrd, Hash, Eq, Ord)]
+pub struct WriterDataLifecycle {
+    autodispose_unregistered_instances: bool,
+}
+
+#[derive(Debug, PartialEq, PartialOrd, Hash, Eq, Ord)]
+pub struct ReaderDataLifecycle {
+    autopurge_nowriter_samples_delay: Duration,
+    autopurge_disposed_samples_delay: Duration,
 }
 
 #[derive(Debug, PartialEq, Hash, Eq, Ord)]
