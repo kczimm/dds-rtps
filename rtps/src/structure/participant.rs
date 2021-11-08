b3b9409a6638a377237bb60a5e442d9ff2fc8ab0
Kevin C. Zimmerman <kevinczimmerman@gmail.com>
2021-11-08 14:55:30 -0600

    update participant module in rtps

--- rtps/src/structure/participant.rs
+++ rtps/src/structure/participant.rs
@@ -1,53 +1,72 @@
-use crate::behavior::cache::VecCache;
-
 use super::{
-    historycache::HistoryCache, EntityId, Guid, Locator, ProtocolVersion, ReliabilityKind,
-    TopicKind,
+    Entity, EntityId, Guid, GuidPrefix, Locator, ProtocolVersion, ReliabilityKind, TopicKind,
+    VendorId,
 };
 
-/// RTPS Participant is the container of RTPS Group entities which contain
-/// Endpoint entities. The RTPS Participant maps to a DDS DomainParticipant.
+/// RTPS [`Participant`] is the container of RTPS [`Group`] entities which
+/// contain [`Endpoint`] entities. The RTPS [`Participant`] maps to a DDS
+/// DomainParticipant.
 #[derive(Debug)]
-pub struct Participant<Cache = VecCache>
-where
-    Cache: HistoryCache,
-{
+pub struct Participant {
     guid: Guid,
+
     protocol_version: ProtocolVersion,
+    vendor_id: VendorId,
     default_unicast_locator_list: Vec<Locator>,
     default_multicast_locator_list: Vec<Locator>,
+    guid_prefix: GuidPrefix,
 
-    publishers: Group<Writer, Cache>,
-    subscribers: Group<Reader, Cache>,
+    publishers: Vec<Publisher>,
+    subscribers: Vec<Subscriber>,
 }
 
+impl Entity for Participant {
+    fn entity(&self) -> Guid {
+        self.guid
+    }
+}
+
+#[derive(Debug)]
+pub struct Group<Kind> {
+    guid: Guid,
+
+    endpoints: Vec<Endpoint<Kind>>,
+}
+
+/// Writer
 #[derive(Debug)]
 pub struct Writer;
 
 #[derive(Debug)]
 pub struct Reader;
 
-#[derive(Debug)]
-pub struct Group<Type, Cache: HistoryCache> {
-    guid: Guid,
-    endpoints: Vec<Endpoint<Type, Cache>>,
+pub type Publisher = Group<Writer>;
+pub type Subscriber = Group<Reader>;
+
+impl<Kind> Entity for Group<Kind> {
+    fn entity(&self) -> Guid {
+        self.guid
+    }
 }
 
 #[derive(Debug)]
-pub struct Endpoint<Type, Cache: HistoryCache> {
+pub struct Endpoint<Kind> {
     guid: Guid,
+
     topic_kind: TopicKind,
     reliability_level: ReliabilityKind,
     unicast_locator_list: Vec<Locator>,
     multicast_locator_list: Vec<Locator>,
     endpoint_group: EntityId,
-    _type: std::marker::PhantomData<Type>,
 
-    cache: Cache,
+    _type: std::marker::PhantomData<Kind>,
 }
 
-impl<Cache: HistoryCache> Endpoint<Writer, Cache> {
-    pub fn new_change(&mut self) {
-        todo!()
+impl<Kind> Entity for Endpoint<Kind> {
+    fn entity(&self) -> Guid {
+        self.guid
     }
 }
+
+impl Endpoint<Writer> {}
+impl Endpoint<Reader> {}
